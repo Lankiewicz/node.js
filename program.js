af1fb173452b6f581223471a3b1e04fbfb7bcef0
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-03-26 18:05:01 +0200

    update

--- program.js
+++ program.js
@@ -1,22 +1,20 @@
+var os = require('os');
+
+process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!');
-        }
-			var information = 'about';
-			switch (information) {
-				case '/exit':
-					process.stdout.write('Quitting app!\n');
-					break;
-				case 'about':
-					console.log(process.env);
-					break;
-			}
+    if(input !== null) {
+        var instruction = input.trim();
+        switch(instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/sayhello':
+                process.stdout.write('hello!\n');
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
     }
 });
